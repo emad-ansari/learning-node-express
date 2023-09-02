2a1efa62b0f1c240835821d6e8ece5a0ec3e45d6
92Mohammad <m.e89571828@gamil.com>
2023-09-02 23:46:33 +0530

    adding new student logic working well

--- public/js/searchStudent.js
+++ public/js/searchStudent.js
@@ -1,12 +1,34 @@
-const studentArray = require('../../data/studentdata')
+let studentArray = require('../../data/studentdata')
+const fs = require('fs');
+
+const studentDataBasePath = './data/studentdatabase.json'
+
+
+
+if (fs.existsSync(studentDataBasePath)) {
+
+    let allStudent = fs.readFileSync(studentDataBasePath,'utf-8' )
+    studentArray = JSON.parse(allStudent)
+
+}
+
+
+
  // below function will find that whether  aparticular student lies in database or not
 const isStudentMatch = (studentName)=> {
+    console.log(' this is inside is studnet function', studentName)
+   
     let studentData = null;
     studentArray.forEach((student)=> {
+    
         if (student.name === studentName){
-            studentData = student;
+
+            studentData = student
         }
+       
     });
+  
+   
 
     const unknownStudent = {
         name: "unknown",
--- server.js
+++ server.js
@@ -2,38 +2,57 @@ const express= require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const PORT = 3000;
-const path = require('path');
+const fs  = require('fs')
 const findStudent = require('./public/js/searchStudent')
-const studentdatabase = require('./data/studentdata')
-// console.log(studentdatabase)
+let studentdatabase = require('./data/studentdata')
+
 
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
+const studentDataBasePath = __dirname + '/data/studentdatabase.json'
+
+if (fs.existsSync(studentDataBasePath)){
+    const fileData = fs.readFileSync(studentDataBasePath, 'utf-8')
+    // console.log('this is type of file data ',typeof fileData)
+    studentdatabase = JSON.parse(fileData)
+}
+
+
+app.post('/api/addstudent', (req, res)=> {
+    const newStudentData = req.body;
+    // console.log('student data recieved: ', newStudentData)
+    studentdatabase.push(newStudentData)
+    // update the studentDataBase json file 
+    const formattData =  JSON.stringify(studentdatabase, null, '\n')
+    fs.writeFileSync(studentDataBasePath, formattData)
+    console.log('updated student database:  ',studentdatabase)
+    res.status(200).send('Student data received');
+});
+
 
 app.get('/api/:name', (req, res)=> {
     
-    const studentName = req.params.name.toLowerCase();
-    if (studentName === 'all'){
+    let studentName = req.params.name;
+    // make the first character to uppercase
+    studentName = studentName.charAt(0).toUpperCase() + studentName.slice(1);
+    
+    if (studentName === 'All'){
         // respond with all student 
-        res.json(studentdatabase)
+        res.status(200).json(studentdatabase)
     }
     else {
         // respond for a asking student
+       
         const studentObject = findStudent(studentName)
-        res.json(studentObject)
+
+        res.status(200).json(studentObject)
     }
  
 });
 
-app.post('/api/addstudent', (req, res)=> {
-    const newStudentData = req.body;
-    console.log(newStudentData)
 
-    studentdatabase.push(newStudentData)
-    res.status(200).send('Student data received');
-});
 
 app.delete('/api/deletestudent', (req, res)=> {
     const student_roll = req.body.student_roll;
