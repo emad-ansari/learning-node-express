386d2839be4f6689358979853079c3f1d5a884b8
92Mohammad <m.e89571828@gamil.com>
2023-09-12 23:29:43 +0530

    learnt express middleware

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express= require('express');
 const app = express();
+const path = require('path')
 const PORT = 3000;
+
+// using the middleware logger function inside server
+// app.use(logger)
+
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+
+
+
+app.get('/user', auth, (req, res)=> {
+    res.send('USERS PAGE')
+})
+
+// express middleware example 
+function logger(req, res, next){
+    console.log(new Date)
+    next()
+}
+
+// authentication middleware
+function auth (req, res, next){
+    console.log('this is my authentication middleware')
+    next()
+}
+
+
 
 
 
